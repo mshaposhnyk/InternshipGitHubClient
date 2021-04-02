f5788ffbbbb9347bef3691df602b391e21234bf1
Maxym Shaposhnyk <mshaposhnyk@griddynamics.com>
2021-04-02 17:26:09 +0300

    Implemented dependency injection with local database

--- app/src/main/java/com/example/internshipgithubclient/di/DataModules.kt
+++ app/src/main/java/com/example/internshipgithubclient/di/DataModules.kt
@@ -2,47 +2,87 @@ package com.example.internshipgithubclient.di
 
 import com.example.core.data.*
 import com.example.core.interactors.*
+import com.example.internshipgithubclient.db.issue.IssueDao
+import com.example.internshipgithubclient.db.issue.RoomIssueDataSource
+import com.example.internshipgithubclient.db.pull.PullDao
+import com.example.internshipgithubclient.db.pull.RoomPullDataSource
+import com.example.internshipgithubclient.db.repo.RepoDao
+import com.example.internshipgithubclient.db.repo.RoomRepoDataSource
+import com.example.internshipgithubclient.db.user.RoomUserDataSource
+import com.example.internshipgithubclient.db.user.UserDao
 import com.example.internshipgithubclient.network.repo.RepoApiService
-import com.example.internshipgithubclient.network.repo.RestIssueDataSource
-import com.example.internshipgithubclient.network.repo.RestPullDataSource
-import com.example.internshipgithubclient.network.repo.RestRepoDataSource
-import com.example.internshipgithubclient.network.user.RestUserDataSource
+import com.example.internshipgithubclient.network.repo.RestRemoteIssueDataSource
+import com.example.internshipgithubclient.network.pull.RestRemotePullDataSource
+import com.example.internshipgithubclient.network.repo.RestRemoteRepoDataSource
+import com.example.internshipgithubclient.network.user.RestRemoteUserDataSource
 import com.example.internshipgithubclient.network.user.UserApiService
 import dagger.Module
 import dagger.Provides
 
 @Module
-class DataSourcesModule {
+class LocalDataSourcesModule {
     @Provides
-    fun provideUserDataSource(userApiService: UserApiService): UserDataSource =
-        RestUserDataSource(userApiService)
+    fun provideUserDataSource(userDao: UserDao): LocalUserDataSource = RoomUserDataSource(userDao)
 
     @Provides
-    fun provideRepoDataSource(repoApiService: RepoApiService): RepoDataSource =
-        RestRepoDataSource(repoApiService)
+    fun provideRepoDataSource(repoDao: RepoDao, userDao: UserDao): LocalRepoDataSource =
+        RoomRepoDataSource(repoDao, userDao)
 
     @Provides
-    fun provideIssueDataSource(repoApiService: RepoApiService): IssueDataSource =
-        RestIssueDataSource(repoApiService)
+    fun provideIssueDataSource(issueDao: IssueDao, userDao: UserDao): LocalIssueDataSource =
+        RoomIssueDataSource(issueDao, userDao)
 
     @Provides
-    fun providePullDataSource(repoApiService: RepoApiService): PullDataSource =
-        RestPullDataSource(repoApiService)
+    fun providePullDataSource(pullDao: PullDao, userDao: UserDao): LocalPullDataSource =
+        RoomPullDataSource(pullDao, userDao)
+}
+
+@Module
+class RemoteDataSourcesModule {
+    @Provides
+    fun provideUserDataSource(userApiService: UserApiService): RemoteUserDataSource =
+        RestRemoteUserDataSource(userApiService)
+
+    @Provides
+    fun provideRepoDataSource(repoApiService: RepoApiService): RemoteRepoDataSource =
+        RestRemoteRepoDataSource(repoApiService)
+
+    @Provides
+    fun provideIssueDataSource(repoApiService: RepoApiService): RemoteIssueDataSource =
+        RestRemoteIssueDataSource(repoApiService)
+
+    @Provides
+    fun providePullDataSource(repoApiService: RepoApiService): RemotePullDataSource =
+        RestRemotePullDataSource(repoApiService)
 }
 
 @Module
 class RepositoriesModule {
     @Provides
-    fun provideRepoRepository(repoDataSource: RepoDataSource) = RepoRepository(repoDataSource)
+    fun provideRepoRepository(
+        remoteRepoDataSource: RemoteRepoDataSource,
+        localRepoDataSource: LocalRepoDataSource
+    ) = RepoRepository(remoteRepoDataSource, localRepoDataSource)
 
     @Provides
-    fun provideUserRepository(userDataSource: UserDataSource) = UserRepository(userDataSource)
+    fun provideUserRepository(
+        remoteUserDataSource: RemoteUserDataSource,
+        localUserDataSource: LocalUserDataSource
+    ) = UserRepository(remoteUserDataSource, localUserDataSource)
 
     @Provides
-    fun providesPullRepository(pullDataSource: PullDataSource) = PullRepository(pullDataSource)
+    fun providesPullRepository(
+        remotePullDataSource: RemotePullDataSource,
+        localPullDataSource: LocalPullDataSource,
+        localUserDataSource: LocalUserDataSource
+    ) = PullRepository(remotePullDataSource, localPullDataSource, localUserDataSource)
 
     @Provides
-    fun providesIssueRepository(issueDataSource: IssueDataSource) = IssueRepository(issueDataSource)
+    fun providesIssueRepository(
+        remoteIssueDataSource: RemoteIssueDataSource,
+        localIssueDataSource: LocalIssueDataSource,
+        localUserDataSource: LocalUserDataSource
+    ) = IssueRepository(remoteIssueDataSource, localIssueDataSource, localUserDataSource)
 }
 
 @Module
