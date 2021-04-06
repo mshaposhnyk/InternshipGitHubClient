13e0989bcb325b332f72918737e73e1cf8243d5d
Maxym Shaposhnyk <mshaposhnyk@griddynamics.com>
2021-04-06 16:52:15 +0300

    Interactors now handle errors and provide Result entity.

--- core/src/main/java/com/example/core/interactors/GetRepoIssues.kt
+++ core/src/main/java/com/example/core/interactors/GetRepoIssues.kt
@@ -1,8 +1,22 @@
 package com.example.core.interactors
 
 import com.example.core.data.IssueRepository
+import com.example.core.data.NetworkErrorHandler
+import com.example.core.domain.ErrorHandler
+import com.example.core.domain.Issue
 import com.example.core.domain.Repo
+import kotlinx.coroutines.flow.Flow
+import com.example.core.domain.Result
 
-class GetRepoIssues(private val issueRepository: IssueRepository) {
-    operator fun invoke(repo: Repo) = issueRepository.getRepoIssues(repo)
+class GetRepoIssues(
+    private val issueRepository: IssueRepository,
+    private val errorHandler: ErrorHandler = NetworkErrorHandler()
+) {
+    suspend operator fun invoke(repo: Repo): Result<Flow<Issue>> {
+        return try {
+            Result.Success(issueRepository.getRepoIssues(repo))
+        } catch (ex: Throwable) {
+            Result.Error(errorHandler.getError((ex)))
+        }
+    }
 }
--- core/src/main/java/com/example/core/interactors/GetRepoPulls.kt
+++ core/src/main/java/com/example/core/interactors/GetRepoPulls.kt
@@ -1,8 +1,22 @@
 package com.example.core.interactors
 
+import com.example.core.data.NetworkErrorHandler
 import com.example.core.data.PullRepository
+import com.example.core.domain.ErrorHandler
+import com.example.core.domain.Pull
 import com.example.core.domain.Repo
+import com.example.core.domain.Result
+import kotlinx.coroutines.flow.Flow
 
-class GetRepoPulls(private val pullRepository: PullRepository) {
-    operator fun invoke(repo: Repo)=pullRepository.getRepoPulls(repo)
+class GetRepoPulls(
+    private val pullRepository: PullRepository,
+    private val errorHandler: ErrorHandler = NetworkErrorHandler()
+) {
+    suspend operator fun invoke(repo: Repo): Result<Flow<Pull>> {
+        return try {
+            Result.Success(pullRepository.getRepoPulls(repo))
+        } catch (ex: Throwable){
+            Result.Error(errorHandler.getError((ex)))
+        }
+    }
 }
